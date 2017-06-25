a4ab439f15f014a3ef9b2db0d7aae6b5401b98fc
Matt Stammers <matthewstammers@googlemail.com>
2017-06-25 17:25:00 +0100

    Update Removing min() & max() from an array.py

--- Simple Functions/Removing min() & max() from an array.py
+++ Simple Functions/Removing min() & max() from an array.py
@@ -4,3 +4,16 @@ def sum_array(arr): # removes outliers and handles tiny arrays
     if arr == None or len(arr) < 3:
         return 0
     return sum(arr) - max(arr) - min(arr)
+
+def sum_array(arr): # another way of doing it
+    return sum(sorted(arr)[1:-1]) if arr and len(arr) > 1 else 0
+
+def sum_array(arr):
+    if arr==None or len(arr)==0 or len(arr)==1:return 0
+    return (sum(arr)-max(arr)-min(arr))
+
+def sum_array(arr):
+    if arr == [] or arr == None or len(arr) == 1:
+        return 0
+    else:
+        return (- (max(arr) + min(arr)) + sum(arr))

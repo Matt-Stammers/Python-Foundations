97210e801675ba76131c52e4dd098ccaf73870f5
Matt Stammers <matthewstammers@googlemail.com>
2017-07-17 22:52:40 +0100

    Create Integer_Multiplication.py

---
+++ Simple Functiions/Integer_Multiplication.py
@@ -0,0 +1,18 @@
+# Another really simple function to calculate how much water one must drink when cycling in this example
+
+def litres(time):
+    return int(time*0.5)
+    
+# or even smarter
+
+def litres(time):
+    return int(time*0.5)
+    
+# or you can use floor division!
+    
+import math
+def litres(time):
+    return math.floor(.50 * time)
+    
+def litres(total_time):
+    return total_time // 2

922dbdd874d490c77fc4c2677619d75ae504fb9c
Matt Stammers <matthewstammers@googlemail.com>
2017-09-23 15:56:59 +0100

    Create Super_ball.py

---
+++ Object_Oriented_Programming/Super_ball.py
@@ -0,0 +1,11 @@
+# very simple object - the super_ball!
+
+class Ball(object):
+    def __init__(self, ball_type ='regular'):
+        self.ball_type = ball_type
+        
+class Ball(object):
+    def __init__(self, type = "regular"):
+        self.ball_type = type
+        
+Ball('super').ball_type

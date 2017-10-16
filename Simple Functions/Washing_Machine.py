7e3b30bbad6d8944483e1cabfcc570f8ccadcf0f
Matt Stammers <matthewstammers@googlemail.com>
2017-10-16 11:56:29 +0100

    Update Washing_Machine.py

--- Simple Functions/Washing_Machine.py
+++ Simple Functions/Washing_Machine.py
@@ -12,6 +12,23 @@ def how_much_water(water, clothes, load):
         water *= 1.1
 
     return round(water, 2)
-    
+
+# or: 
+
 def how_much_water(l, x, n):
     return "Too much clothes" if n > 2*x else "Not enough clothes" if n < x else round(1.1**(n-x)*l, 2)
+
+# this highlights the importance of making your code easy to understand: I started trying to solve it like so:
+
+def how_much_water(L,X,N):
+  if N > 2*X:
+    return 'Too much clothes'
+  elif N < X:
+    return 'Not enough clothes'
+  else:
+    for i in range(N-X):
+      L = L * 1.1
+      
+  return round(L, 2)
+
+# this was conceptually much harder to understand.
